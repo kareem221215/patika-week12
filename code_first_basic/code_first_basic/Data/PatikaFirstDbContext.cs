1ad3934694e6d64e5afd4fa54f2d354aaac81e1e
kareem <120470069+kareem221215@users.noreply.github.com>
2025-03-11 07:01:31 +0300

    Add files via upload

---
+++ code_first_basic/code_first_basic/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using code_first_basic.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace code_first_basic.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=PatikaCodeFirstDb1;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ code_first_basic/code_first_basic/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace code_first_basic.Models
+{
+    public class Game
+    {  
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
+
