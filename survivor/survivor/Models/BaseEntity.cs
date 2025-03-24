e7de4b191f64521251d8ee3edd8cc3dc51ddc71b
kareem <120470069+kareem221215@users.noreply.github.com>
2025-03-24 20:19:05 +0300

    Add files via upload

---
+++ survivor/survivor/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace survivor.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ survivor/survivor/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace survivor.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Competitor> Competitors { get; set; }
+    }
+}
