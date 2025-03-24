0887f58558a7f6c5fe494c01b6c83e0a8eaac4a2
kareem <120470069+kareem221215@users.noreply.github.com>
2025-03-24 20:10:59 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using survivor.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace survivor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Competitor> Competitors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Competitor.cs
@@ -0,0 +1,10 @@
+﻿namespace survivor.Models
+{
+    public class Competitor : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
